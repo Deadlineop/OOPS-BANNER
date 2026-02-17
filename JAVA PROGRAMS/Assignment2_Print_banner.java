565ac34df16c17cc34bb6cf644b3dddf884e13e8
Deadlineop <sm07102007@gmail.com>
2026-02-17 10:50:58 +0530

    [Deadlineop} Added:OOPSBannerApp

---
+++ JAVA PROGRAMS/Assignment2_Print_banner.java
@@ -0,0 +1,12 @@
+public class Assignment2_Print_banner {
+    public static void main(String[] args)
+    {
+        System.out.println("    ***    " + "    ***     " + "******   "+"   ***** ");
+        System.out.println(" **     ** " + " **     **  " + "**    ** "+" **      ");
+        System.out.println("**       **" + "**       ** " + "**    ** "+"**       ");
+        System.out.println("**       **" + "**       ** " + "*****    "+"  *****  ");
+        System.out.println("**       **" + "**       ** " + "**       "+"      ** ");
+        System.out.println(" **     ** " + " **     **  " + "**       "+" **   ** ");
+        System.out.println("    ***    " + "    ***     " + "**       "+"   ***** ");
+    }
+}
